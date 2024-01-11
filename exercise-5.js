01d8bde1eab987bdfd15d9efaf17f4d5c54165c0
I3AIVK <wacharapol.kh@gmail.com>
2024-01-11 14:52:22 +0700

    feat:add complete assignments

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,12 @@
-// Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: {
+        month: "$created_at",
+        year: "$created_at",
+        total_sales: "$total_price",
+      },
+    },
+  },
+  { $sort: { _id: -1 } },
+]);
