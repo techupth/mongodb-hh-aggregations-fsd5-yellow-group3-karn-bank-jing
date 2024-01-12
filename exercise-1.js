ee849a085d821929f1d3df1f845a1d7c98bdd331
พุฒิพงศ์ วราศรีทรัพย์ <kitsune@Kitsus-MacBook.local>
2024-01-12 07:55:17 +0700

    AssignmentsV1.0

--- exercise-1.js
+++ exercise-1.js
@@ -1,1 +1,8 @@
-// Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: "$credit_card_type",
+      totalPrice: { $sum: "$total_price" },
+    },
+  },
+]);
--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,9 @@
-// Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { year: { $year: "$created_at" }, month: { $month: "$created_at" } },
+      total_price_by_month: { $sum: "$total_price" },
+    },
+  },
+  { $sort: { _id: -1 } },
+]);
